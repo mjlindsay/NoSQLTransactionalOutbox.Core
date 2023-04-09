13ad6a68fb532f9b6767a764ace22e3ba233d192
FissionTech <michael.lindsay.j@gmail.com>
2023-04-09 17:42:57 -0400

    Added MediatR

--- NoSQLTransactionalOutbox.Core/Event/IEvent.cs
+++ NoSQLTransactionalOutbox.Core/Event/IEvent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace NoSQLTransactionalOutbox.Core.Event
 {
-    public interface IEvent
+    public interface IEvent : INotification
     {
         public string Id { get; }
 
